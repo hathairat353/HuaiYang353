8dc49a9d631ec5842d4f19b048706b7c173cd492
hathairat353 <2671052741353@rmutr.ac.th>
2025-02-06 10:04:31 +0700

    upload web map

---
+++ layers/layers.js
@@ -0,0 +1,60 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:3857").setExtent([11081284.738205, 1294242.506924, 11115792.446782, 1310135.887809]);
+var wms_layers = [];
+
+
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+var format_353_1 = new ol.format.GeoJSON();
+var features_353_1 = format_353_1.readFeatures(json_353_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_353_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_353_1.addFeatures(features_353_1);
+var lyr_353_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_353_1, 
+                style: style_353_1,
+                popuplayertitle: 'ต.ห้วยยาง 353',
+                interactive: true,
+    title: 'ต.ห้วยยาง 353<br />\
+    <img src="styles/legend/353_1_0.png" /> All\'s day cafe&Restaurant<br />\
+    <img src="styles/legend/353_1_1.png" /> Hello mango cafe<br />\
+    <img src="styles/legend/353_1_2.png" /> Jack Sparrow\'s Restaurant&coffee<br />\
+    <img src="styles/legend/353_1_3.png" /> lnfinity Beach<br />\
+    <img src="styles/legend/353_1_4.png" /> NYFLKET<br />\
+    <img src="styles/legend/353_1_5.png" /> กาแฟ เกาะจัน<br />\
+    <img src="styles/legend/353_1_6.png" /> ครัวมะปรางเปรี้ยว<br />\
+    <img src="styles/legend/353_1_7.png" /> ชุลมุน coffee<br />\
+    <img src="styles/legend/353_1_8.png" /> ทุ่งยาวคาเฟ่<br />\
+    <img src="styles/legend/353_1_9.png" /> บ้านสวนอินทรีย์อยู่อย่างพอเพียง<br />\
+    <img src="styles/legend/353_1_10.png" /> ป้ารุณ อาหารและเครื่องดื่ม<br />\
+    <img src="styles/legend/353_1_11.png" /> เมมโมรี่ คาเฟ่แคมป์<br />\
+    <img src="styles/legend/353_1_12.png" /> ร้านกาแฟอินทนิล<br />\
+    <img src="styles/legend/353_1_13.png" /> ร้านอาหารกบซีฟู้ด<br />\
+    <img src="styles/legend/353_1_14.png" /> ลองดูคอฟฟี่<br />\
+    <img src="styles/legend/353_1_15.png" /> วิสาหกิจชุมชนท่องเที่ยวเชิงเกษตรและโฮมสเตย์ โดย ชุมชนบ้านห้วยยาง<br />\
+    <img src="styles/legend/353_1_16.png" /> วิสาหกิจชุมชนแปลงใหญ่สหกรณ์โคนมไทย-เดนมาร์ค บ้านเนินดินแดง จำกัด<br />\
+    <img src="styles/legend/353_1_17.png" /> สองพี่น้อง Rose Garden1 coffee&DrinkBreakfast<br />\
+    <img src="styles/legend/353_1_18.png" /> หาดวนกร<br />\
+    <img src="styles/legend/353_1_19.png" /> อุทยานแห่งชาติน้ำตกห้วยยาง<br />\
+    <img src="styles/legend/353_1_20.png" /> <br />'
+        });
+
+lyr_GoogleSatellite_0.setVisible(true);lyr_353_1.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_353_1];
+lyr_353_1.set('fieldAliases', {'ลำดับ': 'ลำดับ', 'ชื่อสถานที่': 'ชื่อสถานที่', 'ละติจูด': 'ละติจูด', 'ลองจิจูด': 'ลองจิจูด', 'ประเภทสถานที่': 'ประเภทสถานที่', 'ที่อยู่': 'ที่อยู่', 'ตำบล': 'ตำบล', 'อำเภอ': 'อำเภอ', 'จังหวัด': 'จังหวัด', 'ภาพสถานที่': 'ภาพสถานที่', 'Facebook': 'Facebook', 'field_12': 'field_12', 'field_13': 'field_13', 'field_14': 'field_14', 'field_15': 'field_15', 'field_16': 'field_16', 'field_17': 'field_17', 'field_18': 'field_18', 'field_19': 'field_19', 'field_20': 'field_20', 'field_21': 'field_21', 'field_22': 'field_22', 'field_23': 'field_23', 'field_24': 'field_24', 'field_25': 'field_25', });
+lyr_353_1.set('fieldImages', {'ลำดับ': 'Range', 'ชื่อสถานที่': 'TextEdit', 'ละติจูด': 'TextEdit', 'ลองจิจูด': 'TextEdit', 'ประเภทสถานที่': 'TextEdit', 'ที่อยู่': 'TextEdit', 'ตำบล': 'TextEdit', 'อำเภอ': 'TextEdit', 'จังหวัด': 'TextEdit', 'ภาพสถานที่': 'TextEdit', 'Facebook': 'TextEdit', 'field_12': 'TextEdit', 'field_13': 'TextEdit', 'field_14': 'TextEdit', 'field_15': 'TextEdit', 'field_16': 'TextEdit', 'field_17': 'TextEdit', 'field_18': 'TextEdit', 'field_19': 'TextEdit', 'field_20': 'TextEdit', 'field_21': 'TextEdit', 'field_22': 'TextEdit', 'field_23': 'TextEdit', 'field_24': 'TextEdit', 'field_25': 'TextEdit', });
+lyr_353_1.set('fieldLabels', {'ลำดับ': 'no label', 'ชื่อสถานที่': 'header label - always visible', 'ละติจูด': 'no label', 'ลองจิจูด': 'no label', 'ประเภทสถานที่': 'header label - always visible', 'ที่อยู่': 'no label', 'ตำบล': 'header label - always visible', 'อำเภอ': 'header label - always visible', 'จังหวัด': 'header label - always visible', 'ภาพสถานที่': 'header label - always visible', 'Facebook': 'inline label - visible with data', 'field_12': 'no label', 'field_13': 'no label', 'field_14': 'no label', 'field_15': 'no label', 'field_16': 'no label', 'field_17': 'no label', 'field_18': 'no label', 'field_19': 'no label', 'field_20': 'no label', 'field_21': 'no label', 'field_22': 'no label', 'field_23': 'no label', 'field_24': 'no label', 'field_25': 'no label', });
+lyr_353_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/353_1_style.js
@@ -0,0 +1,221 @@
+var size = 0;
+var placement = 'point';
+function categories_353_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'All\'s day cafe&Restaurant':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(208,222,114,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Hello mango cafe':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(219,100,136,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Jack Sparrow\'s Restaurant&coffee':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(64,64,239,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'lnfinity Beach':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(155,72,219,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'NYFLKET':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(104,228,174,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'กาแฟ เกาะจัน':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(207,50,50,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ครัวมะปรางเปรี้ยว':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(132,95,225,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ชุลมุน coffee':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(209,29,185,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ทุ่งยาวคาเฟ่':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(52,228,52,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'บ้านสวนอินทรีย์อยู่อย่างพอเพียง':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(227,154,58,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ป้ารุณ อาหารและเครื่องดื่ม':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(92,201,220,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'เมมโมรี่ คาเฟ่แคมป์':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(50,220,98,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ร้านกาแฟอินทนิล':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(232,111,64,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ร้านอาหารกบซีฟู้ด':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(138,219,106,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ลองดูคอฟฟี่':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(231,206,62,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'วิสาหกิจชุมชนท่องเที่ยวเชิงเกษตรและโฮมสเตย์ โดย ชุมชนบ้านห้วยยาง':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(130,190,236,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'วิสาหกิจชุมชนแปลงใหญ่สหกรณ์โคนมไทย-เดนมาร์ค บ้านเนินดินแดง จำกัด':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(148,211,65,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'สองพี่น้อง Rose Garden1 coffee&DrinkBreakfast':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(25,237,209,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'หาดวนกร':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(226,42,149,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'อุทยานแห่งชาติน้ำตกห้วยยาง':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(70,111,215,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.399999999999999 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(189,116,202,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_353_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("ชื่อสถานที่");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 0.6000000000000001;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("ชื่อสถานที่") !== null) {
+        labelText = String(feature.get("ชื่อสถานที่"));
+    }
+    
+    var style = categories_353_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
